bac1e3754eb49389411fa95f6e805b08eb986dd6
yedidiakwame <yedidiakwame@gmail.com>
2026-05-22 21:43:12 +0200

    Feature add CI pipeline

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log('Hello DevOps!');
+console.log("I'm learning CI/CD using GitHub Actions")
---
+++ test.js
@@ -0,0 +1,3 @@
+console.log('Starting tests...');
+setTimeout( () => console.log('Waiting 3 seconds...'), 3000);
+console.log('tests complete!');
